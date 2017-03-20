6aa0525c69002bdeaf9719480aac44b2faf15072
Denise Gemesio <denise.gemesio@hotmail.com>
2017-03-20 11:56:55 +0100

    before adding part2

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/BasicStreamProcessor.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/BasicStreamProcessor.java
@@ -7,28 +7,34 @@ import java.io.Reader;
 import java.io.Writer;
 
 /**
- * The responsibility of this class is to read characters from an input
- * reader, to process them, and to write them to an output writer.
- * 
+ * The responsibility of this class is to read characters from an input reader, to
+ * process them, and to write them to an output writer.
+ *
  * It is called "basic" stream processor, because it does nothing with the
  * characters: it writes them without transforming them.
- * 
- * Note: clients do not instantiate this class directly. They obtain instances
- * of this class via the StreamProcessorsFactory.
- * 
+ *
+ * Note: clients do not instantiate this class directly. They obtain instances of
+ * this class via the StreamProcessorsFactory.
+ *
  * @author Olivier Liechti
  */
 class BasicStreamProcessor implements IStreamProcessor {
 
-  @Override
-  public void process(Reader in, Writer out) throws IOException {
-    BufferedReader br = new BufferedReader(in);
-    BufferedWriter bw = new BufferedWriter(out);
-    int c = br.read();
-    while (c != -1) {
-      c = br.read();
-    }
-    bw.flush();
-  }
+   @Override
+   public void process(Reader in, Writer out) throws IOException {
+      
+      BufferedReader br = new BufferedReader(in);
+      BufferedWriter bw = new BufferedWriter(out);
+      
+      int c = br.read();
+      
+      while (c != -1) {
+         // élimination des 'e' et 'E'
+         if ((char)c != 'e' && (char)c != 'E') {
+            bw.write(c);
+         }
+      }
+      bw.flush();
+   }
 
 }
